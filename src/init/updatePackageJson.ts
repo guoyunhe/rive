e9656bdd77fb5aa6e2010322280631e477cb5b9f
Guo Yunhe <i@guoyunhe.me>
2022-03-13 10:23:38 +0800

    feat: add stylelint config

--- src/init/updatePackageJson.ts
+++ src/init/updatePackageJson.ts
@@ -26,6 +26,7 @@ const config = {
   },
   eslintIgnore: ['node_modules', 'coverage', 'dist'],
   stylelint: {
+    extends: ['stylelint-config-rive'],
     ignoreFiles: ['node_modules', 'coverage', 'dist'],
   },
 };
