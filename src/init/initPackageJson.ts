1e94d7610f009d007ce11f39584ca2af43ab8989
Guo Yunhe <i@guoyunhe.me>
2022-06-11 18:47:35 +0800

    refactor: init package.json

--- src/init/initPackageJson.ts
+++ src/init/initPackageJson.ts
@@ -1,10 +1,35 @@
 import fse from 'fs-extra';
 import { merge, unset } from 'lodash-es';
-import packageJsonConfig from '../config/package-json.js';
 
 const { readJsonSync, writeJsonSync } = fse;
 
 const filePath = './package.json';
+
+const packageJsonConfig = {
+  main: 'dist/index.cjs.js',
+  module: 'dist/index.esm.js',
+  style: 'dist/index.css',
+  types: 'dist/index.d.ts',
+  files: ['dist', 'README.md'],
+  scripts: {
+    start: 'rive start',
+    build: 'rive build',
+    test: 'rive test',
+    lint: 'rive lint',
+    format: 'rive format',
+  },
+  devDependencies: {
+    rive: 'latest',
+  },
+  eslintConfig: {
+    extends: ['rive'],
+  },
+  eslintIgnore: ['node_modules', 'coverage', 'dist'],
+  stylelint: {
+    extends: ['stylelint-config-rive'],
+    ignoreFiles: ['node_modules', 'coverage', 'dist'],
+  },
+};
 
 const devDepsToRemove = [
   'vite',
