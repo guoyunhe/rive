b80000e87e4253ab52e4ea05a3dbea36cd8c0988
鹤仙 <guoyunhe.gyh@alibaba-inc.com>
2022-09-14 17:35:08 +0800

    fix: jest coverage

--- jest-preset.js
+++ jest-preset.js
@@ -1,7 +1,22 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 const config = {
-  preset: 'ts-jest',
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/**/*.d.ts',
+  ],
+  coveragePathIgnorePatterns: [
+    '/build/',
+    '/dist/',
+    '/coverage/',
+    '/node_modules/',
+  ],
+  coverageReporters: ['html', 'json-summary', 'text'],
+  moduleNameMapper: {
+    'lodash-es': 'lodash',
+    'fetch-mock/esm/client': 'fetch-mock/cjs/client',
+  },
   testEnvironment: 'jsdom',
+  transform: { '^.+\\.[tj]sx?$': 'ts-jest' },
 };
 
 export default config;
